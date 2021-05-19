eaf4e9e9e5ffce72d573f69d037411731bd14097
WTFHCN <840980415@qq.com>
2021-05-19 13:07:59 +0800

    ImageSearch 0.5

---
+++ FindImage.py
@@ -0,0 +1,69 @@
+
+
+from PIL import Image
+import argparse as ap
+import cv2
+import numpy as np
+import joblib
+from scipy.cluster.vq import *
+
+from sklearn import preprocessing
+import numpy as np
+
+from pylab import *
+
+
+def add(x, y):
+    return x+y
+
+
+def search_image(image_path):
+    mpl.rcParams['font.sans-serif'] = ['SimHei']
+    # Load the classifier, class names, scaler, number of clusters and vocabulary
+    im_features, image_paths, idf, numWords, voc = joblib.load("bow.pkl")
+
+    # Create feature extraction and keypoint detector objects
+    sift_det = cv2.xfeatures2d.SIFT_create()
+    # List where all the descriptors are stored
+    des_list = []
+
+    im = cv2.imread(image_path)
+    gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    kp, des = sift_det.detectAndCompute(gray, None)
+
+    des_list.append((image_path, des))
+
+    # Stack all the descriptors vertically in a numpy array
+    descriptors = des_list[0][1]
+
+    #
+    test_features = np.zeros((1, numWords), "float32")
+    words, distance = vq(descriptors, voc)
+    for w in words:
+        test_features[0][w] += 1
+
+    # Perform Tf-Idf vectorization and L2 normalization
+    test_features = test_features*idf
+    test_features = preprocessing.normalize(test_features, norm='l2')
+
+    score = np.dot(test_features, im_features.T)
+
+    rank_ID = np.argsort(-score)  # 从大到小的索引
+
+    # Visualize the results
+    # figure('基于OpenCV的图像检索')
+    # subplot(5, 5, 1)
+    # title('目标图片')
+    # imshow(im[:, :, ::-1])
+    # axis('off')
+
+   # print(rand_ID[0][0])
+
+    for i, ID in enumerate(rank_ID[0][0:1]):
+        return image_paths[ID]
+        # gray()
+        # subplot(5, 5, i+6)
+        # imshow(img)
+        # title('第%d相似' % (i+1))
+        # axis('off')
+    # show()
---
+++ View.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import FindImage
+
+
+class PictureSearch(QWidget):
+    def __init__(self):
+        super(PictureSearch, self).__init__()
+        self.resize(800, 500)
+        self.setWindowTitle("PictureSearch")
+        self.imgName = ""
+        self.choose_lable = QLabel(self)
+        self.choose_lable.resize(300, 300)
+        self.choose_lable.move(80, 100)
+        self.choose_lable.setStyleSheet("QLabel{background:white;}"
+                                        "QLabel{color:rgb(300,300,300);font-size:10px;font-weight:bold;font-family:宋体;}"
+                                        )
+        self.search_lable = QLabel(self)
+        self.search_lable.resize(300, 300)
+        self.search_lable.move(480, 100)
+        self.search_lable.setStyleSheet("QLabel{background:white;}"
+                                        "QLabel{color:rgb(300,300,300);font-size:10px;font-weight:bold;font-family:宋体;}"
+                                        )
+
+        self.choose_button = QPushButton(self)
+        self.choose_button.setText("打开图片")
+        self.choose_button.move(180, 30)
+        self.choose_button.clicked.connect(self.choose_image)
+
+        self.search_button = QPushButton(self)
+        self.search_button.setText("检索图片")
+        self.search_button.move(550, 30)
+
+        self.search_button.clicked.connect(self.search_image)
+
+    def choose_image(self):
+        self.imgName, imgType = QFileDialog.getOpenFileName(
+            self, "打开图片", "", "*.jpg;;*.png;;All Files(*)")
+        image = QtGui.QPixmap(self.imgName).scaled(
+            self.choose_lable.width(), self.choose_lable.height())
+        self.choose_lable.setPixmap(image)
+
+    def search_image(self):
+        find_image = FindImage.search_image(self.imgName)
+        image = QtGui.QPixmap(find_image).scaled(
+            self.search_lable.width(), self.search_lable.height())
+        self.search_lable.setPixmap(image)
+        # print(FindImage.search_image(self.imgName))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myshow = PictureSearch()
+    myshow.show()
+    sys.exit(app.exec_())
